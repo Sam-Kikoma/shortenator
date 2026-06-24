90a2a6f1d1815545a0d6d57aac6ebda199be9189
Sam Kikoma <kikomasamson@gmail.com>
2026-06-24 14:13:59 +0200

    chore: update env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 export default () => ({
   environment: process.env.NODE_ENV || 'development',
+  appHost: process.env.APP_HOST,
   redis: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
